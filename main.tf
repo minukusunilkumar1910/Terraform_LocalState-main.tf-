c5412dd24a5de43614c940370657019a346fe5b1
Minuku.SunilKumar <154435816+minukusunilkumar1910@users.noreply.github.com>
2025-07-16 08:31:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "eu-north-1"  # Change this to your desired region
 }
 
+
 resource "aws_instance" "my_ec2" {
   ami = "ami-0914547665e6a707c"  
   instance_type = "t3.micro"
